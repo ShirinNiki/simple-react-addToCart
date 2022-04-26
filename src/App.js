1ab9bb9288641701b5af2b54b2c4473eccec9515
unknown <niki.shirin@gmail.com>
2022-04-26 23:41:11 +0430

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Layout from './container/Layout/Layout';
+import Shopping from './container/shopping/Shopping';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Shopping />
+      </Layout>
     </div>
   );
 }
